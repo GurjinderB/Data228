f93b586610a52f235c12e6ebf9a40246e0e7eebd
Gurjinder <gurjinder.bassi1@hotmail.co.uk>
2023-05-19 16:20:11 +0100

    Introduction to Python functions

---
+++ Functions/functions_intro.py
@@ -0,0 +1,101 @@
+print("\nQ1a\n")
+# Q1a: Write a function which takes in an integer as an argument and returns the divisors of that number as a list
+# e.g. f(12) = [1, 2, 3, 4, 6, 12]
+# hint: range(1, n) returns a collection of the numbers from 1 to n-1
+
+# A1a:
+def divisors(number: int) -> list:
+    factors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            factors.append(i)
+    return factors
+
+
+print("\nQ1b\n")
+# Q1b: Write a function which takes in two integers as arguments and returns true if one of the numbers
+# is a factor of the other, false otherwise
+# (bonus points if you call your previous function within this function
+
+# A1b:
+def factor(a: int, b: int) -> bool:
+    if a in divisors(b):
+        return True
+    elif b in divisors(a):
+        return True
+    else:
+        return False
+
+
+# -------------------------------------------------------------------------------------- #
+
+
+print("\nQ2a\n")
+# Q2a: write a function which takes a letter (as a string) as an input and outputs it's position in the alphabet
+# alphabet = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+#             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+# A2a:
+def position_in_alphabet(letter: str) -> int:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    return alphabet.index(letter.lower())
+
+
+print("\nQ2b\n")
+# Q2b: create a function which takes a persons name as an input string and returns an
+# ID number consisting of the positions of each letter in the name
+# e.g. f("bob") = "1141" as "b" is in position 1 and "o" is in position 14
+
+# A2b:
+def id_creator(name: str) -> str:
+    id = ""
+    for letter in name:
+        id += str(position_in_alphabet(letter.lower()))
+    return id
+
+
+print("\nQ2c\n")
+# Q2c: Create a function which turns this ID into a password. The function should subtract
+# the sum of the numbers in the id that was generated from the whole number of the id.
+# e.g. f("bob") -> 1134 (because bob's id was 1141 and 1+1+4+1 = 7 so 1141 - 7 = 1134)
+
+# A2c:
+def password_creator(ID: str) -> int:
+    sum = 0
+    for i in ID:
+        sum += int(i)
+    return int(ID) - sum
+
+
+# -------------------------------------------------------------------------------------- #
+
+print("\nQ3a\n")
+# Q3a: Write a function which takes an integer as an input, and returns true if the number is prime, false otherwise.
+
+# A3a:
+def is_prime(a) -> bool:
+    for i in range(2, a):
+        if a % i == 0:
+            return False
+    return True
+
+print("\nQ3b\n")
+# Q3b: Now add some functionality to the function which does not error if the user inputs something other than a digit
+
+# A3b:
+def is_prime(a) -> bool:
+    if not isinstance(a, int):
+        return "Input to is_prime() should be an integer"
+    for i in range(2, a):
+        if a % i == 0:
+            return False
+    return True
+
+# -------------------------------------------------------------------------------------- #
+
+
+
+
+
+
