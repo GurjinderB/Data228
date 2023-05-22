96012f6060ad337b36bc7afd42d9eb19f5d15e00
Gurjinder <gurjinder.bassi1@hotmail.co.uk>
2023-05-22 17:38:22 +0100

    Renamed project

---
+++ RestaurantProject/restaurant.py
@@ -0,0 +1,22 @@
+class Table:
+
+    def __init__(sef, diners):
+        self.diners = diners
+
+    def order(item, price, quantity = 1):
+        pass
+
+    def remove(item, price, quantity):
+        if True:
+            return True # if items were on the bill and can be removed
+        else:
+            return False # if quantity on the bill is less than the quantity and make no changes
+        
+    def get_subtotal():
+        pass
+
+    def get_total():
+        pass
+
+    def split_bill():
+        pass
