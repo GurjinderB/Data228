92659ca7f76d51e62f66c35c3bd967718654a27c
Gurjinder <gurjinder.bassi1@hotmail.co.uk>
2023-05-23 10:11:46 +0100

    added comments and character length

--- RestaurantProject/restaurant.py
+++ RestaurantProject/restaurant.py
@@ -4,7 +4,6 @@ class Table:
         self.diners = diners
         self.bill = []
 
-
     def order(self, item: str, price: float, quantity: int = 1) -> None:
         order = {'item': item, 'price': price, 'quantity': quantity}
         ordered_previously = False # check if item already added to bill
@@ -15,7 +14,6 @@ class Table:
                 break
         if not ordered_previously:
             self.bill.append(order)
-
 
     def remove(self, item: str, price: float, quantity: int) -> bool:
         for i, orders in enumerate(self.bill):
@@ -29,20 +27,19 @@ class Table:
             else:
                 return False # quantity < quantity on bill or price or item not on bill
         
-
     def get_subtotal(self) -> float:
         subtotal = 0
         for item in self.bill:
             subtotal += item['price'] * item['quantity']
         return subtotal
 
-
     def get_total(self, tip: float = 0.10) -> dict:
-        service_charge = self.get_subtotal() * tip
-        total = self.get_subtotal() + service_charge
         subtotal = self.get_subtotal()
-        return {'Sub Total': f'£{subtotal:.2f}', 'Service Charge': f'£{service_charge:.2f}', 'Total': f'£{total:.2f}'}     
-
+        service_charge = subtotal * tip
+        total = self.get_subtotal() + service_charge
+        return {'Sub Total': f'£{subtotal:.2f}', 
+                'Service Charge': f'£{service_charge:.2f}', 
+                'Total': f'£{total:.2f}'}
 
     def split_bill(self) -> float:
         return round(self.get_subtotal() / self.diners, 2) 
